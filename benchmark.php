406eaf72810a856e8ba409cde1986ae331162134
Caen De Silva <caen@desilva.se>
2022-06-01 11:12:48 +0200

    Initial Commit

---
+++ benchmark.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * A simple opinionated script to benchmark PHP code.
+ *
+ * I'm not concerned about any overhead added by the helper methods,
+ * as this script is intended to benchmark relative performance
+ * between differing implementations. Since any overhead is
+ * constant for all iterations, it does not affect the
+ * comparative data results I'm interested in.
+ */
+
+class Benchmark {
+	//
+}
