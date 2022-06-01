651ea6293f605ede4ac69431db5c98190a4da7ea
Caen De Silva <caen@desilva.se>
2022-06-01 11:21:50 +0200

    Implement base class

--- benchmark.php
+++ benchmark.php
@@ -11,5 +11,28 @@
  */
 
 class Benchmark {
-	//
+	protected int $iterations;
+    protected float $time_start;
+    protected float $time_end;
+
+	public function __construct(int $iterations)
+    {
+		$this->iterations = $iterations;
+	}
+
+	protected function execute(callable $callback): void
+    {
+		$this->time_start = microtime(true);
+		for ($i = 0; $i < $this->iterations; $i++) {
+			$callback();
+		}
+		$this->time_end = microtime(true);
+	}
+
+	public static function run(callable $callback, int $iterations = 100): Benchmark
+    {
+		$benchmark = new Benchmark($iterations);
+		$benchmark->execute($callback);
+		return $benchmark;
+	}
 }
